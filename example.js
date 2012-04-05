d7ca2542c138a0373e0f19fce337322ccefdf87e
Liu Xiaohui <herolxh@gmail.com>
2012-04-05 23:35:21 +0800

    Update the sample code and correct a error in circular_buffer.js

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 /**
  * Module dependencies.
  */
-
+var inspect = require('util').inspect;
 var mongo = require('mongoskin');
 var db = global.db = mongo.db('localhost/filestore?auto_reconnect=true&poolSize=1');
 
@@ -10,13 +10,18 @@ var connect = require("connect")
 
 db.open(function(err, db){
     var app = connect();
-    app.use(connect.logger('dev'))
-    app.use(connect.static('public'))
-    app.use(connect.json());
-    app.use(connect.urlencoded());
     app.use(multipart());
     app.use(function(req, res){
-        res.end('hello world\n');
-      });
+        if (req.method == 'GET'){
+            res.end('Try POST a file\n');
+            return;
+        }
+        if (req.method == 'POST'){
+            res.end(inspect(req.files)+'\n');
+            return;
+        }
+        res.end('Not implemented');
+    });
     app.listen(3000);
+    console.log('Server is running at port 3000. POST to "/" to upload file');
 })
